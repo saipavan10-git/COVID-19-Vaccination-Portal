5dc525e35ad8c7c38210e8fb1a29c315a9faf616
mayichong <mayichong123@gmail.com>
2022-01-28 16:29:39 -0500

    newFormat

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import './App.css';
 import Homepage from './pages/Homepage';
 import Records from './pages/Records';
-import LoginButton from './components/LoginButton';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
@@ -10,14 +10,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/records" element={<Records/>} />
-        
+        <Route path="/" element={<Homepage />} />
+        <Route path="/records" element={<Records />} />
+
       </Routes>
     </Router>
 
-    // <LoginButton />
-    
+
+
   );
 }
 
