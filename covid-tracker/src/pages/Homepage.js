df4dc90b7b4b9680fa061becf085d869337b7ee6
Yichong Ma <mayichong123@gmail.com>
2022-01-28 19:48:15 -0500

    Update Homepage.js

--- covid-tracker/src/pages/Homepage.js
+++ covid-tracker/src/pages/Homepage.js
@@ -1,43 +1,28 @@
-import React, { Component, Fragment } from 'react';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import LockIcon from '@mui/icons-material/Lock';
+import React from 'react';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-
-class Homepage extends Component {
-    render() {
-        return (
-            <Fragment>
-
-                <div>
-                    <div className='centered'>
-                        <h1>COVID BOOK Homepage</h1>
-                    </div>
-
-                    <div style={{ textAlign: "center" }}>
-                        <Button variant="outlined"
-                            style={{ fontSize: "18px", color: "white", borderColor: "white" }}
-                            component={Link} to="/login"
-                        >Login</Button>
-                        <span style={{ marginLeft: "30px" }}></span>
-                        <Button variant="outlined"
-                            style={{ fontSize: "18px", color: "white", borderColor: "white" }}
-                            component={Link} to="/signup"
-                        >Sign up</Button>
-                    </div>
-
-
+function Homepage() {
+    return (
+        <>
+            <div>
+                <div className='centered'>
+                    <h1>COVID BOOK Homepage</h1>
                 </div>
-
-            </Fragment>
-
-
-        )
-    }
+                <div style={{ textAlign: "center" }}>
+                    <Button variant="outlined"
+                        style={{ fontSize: "18px", color: "white", borderColor: "white" }}
+                        component={Link} to="/login"
+                    >Login</Button>
+                    <span style={{ marginLeft: "30px" }}></span>
+                    <Button variant="outlined"
+                        style={{ fontSize: "18px", color: "white", borderColor: "white" }}
+                        component={Link} to="/signup"
+                    >Sign up</Button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Homepage;
