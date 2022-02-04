04e84377b77d78a63e9bb59b47fe11871c321a7f
venky tanneru <vtanneru@ufl.edu>
2022-02-04 17:01:35 -0500

    Update VaccineList.js

--- covid-tracker/src/pages/VaccineList.js
+++ covid-tracker/src/pages/VaccineList.js
@@ -1,99 +1,100 @@
 import React, { Component, Fragment } from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 export default class VaccineList extends Component {
-    state = {
-        vaccines: [],
-        isLoaded: false,
+  state = {
+    vaccines: [],
+    isLoaded: false,
+  };
+  componentDidMount() {
+    fetch("http://localhost:4000/v1/vaccines")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          vaccines: json.vaccines,
+          isLoaded: true,
+        });
+      });
+  }
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(evt.target);
+    const data = new FormData(evt.target);
+    const payload = Object.fromEntries(data.entries());
+    console.log(payload);
 
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(payload),
     };
-    componentDidMount() {
-        fetch("http://localhost:4000/v1/vaccines")
-            .then((response) => response.json())
-            .then((json) => {
 
-                this.setState({
-                    vaccines: json.vaccines,
-                    isLoaded: true,
-                })
-            })
+    fetch("http://localhost:4000/v1/booking", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  render() {
+    const { vaccines, isLoaded } = this.state;
+    function sayHello(m) {
+      const requestOptions = {
+        method: "POST",
+        body: JSON.stringify(m),
+      };
 
+      fetch("http://localhost:4000/v1/booking", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+      alert(
+        `Congrats! You booked ${m.vaccine_name} vaccine, ${m.vaccine_num}-dose. Please bring your valid ID/Driver's license and your insurance card with you.`
+      );
     }
-    handleSubmit = (evt) => {
-        evt.preventDefault();
-        console.log(evt.target);
-        const data = new FormData(evt.target);
-        const payload = Object.fromEntries(data.entries());
-        console.log(payload);
-
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify(payload),
-        }
-
-        fetch('http://localhost:4000/v1/booking', requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
+    if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <Fragment>
+          <div>
+            <Table style={{ width: "200px" }}>
+              <TableBody>
+                <TableRow>
+                  <TableHead>ID</TableHead>
+                  <TableHead>Vaccine Name</TableHead>
+                  <TableHead>Dose #</TableHead>
+                  <TableHead>State</TableHead>
+                  <TableHead>Zip Code</TableHead>
+                  <TableHead>Appointment</TableHead>
+                </TableRow>
+                {vaccines.map((m) => (
+                  <TableRow key={m.id}>
+                    <TableCell>{m.id}</TableCell>
+                    <TableCell>{m.vaccine_name}</TableCell>
+                    <TableCell>{m.vaccine_num}</TableCell>
+                    <TableCell>{m.state}</TableCell>
+                    <TableCell>{m.zip_code}</TableCell>
+                    <TableCell>
+                      <button
+                        onClick={() => {
+                          sayHello(m);
+                        }}
+                      >
+                        Book
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </Fragment>
+      );
     }
-    render() {
-        const { vaccines, isLoaded } = this.state;
-        function sayHello(m) {
-            const requestOptions = {
-                method: 'POST',
-                body: JSON.stringify(m),
-            }
-
-            fetch('http://localhost:4000/v1/booking', requestOptions)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-
-                })
-            alert(`Congrats! You booked ${m.vaccine_name} vaccine, ${m.vaccine_num}-dose. Please bring your valid ID/Driver's license and your insurance card with you.`)
-        }
-        if (!isLoaded) {
-            return <p>Loading...</p>
-        } else {
-            return (
-                <Fragment>
-                    <div>
-                        <table style={{ width: "200px" }}>
-                            <tbody>
-                                <tr>
-
-                                    <th>ID</th>
-                                    <th>Vaccine Name</th>
-                                    <th>Dose #</th>
-                                    <th>State</th>
-                                    <th>Zip Code</th>
-                                    <th>Appointment</th>
-                                </tr>
-                                {vaccines.map((m) => (
-                                    <tr key={m.id}>
-                                        <td>{m.id}</td>
-                                        <td>{m.vaccine_name}</td>
-                                        <td>{m.vaccine_num}</td>
-                                        <td>{m.state}</td>
-                                        <td>{m.zip_code}</td>
-                                        <td><button onClick={() => { sayHello(m) }}>Book</button></td>
-                                    </tr>
-                                )
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-                </Fragment >
-
-
-
-            );
-        }
-    }
+  }
 }
