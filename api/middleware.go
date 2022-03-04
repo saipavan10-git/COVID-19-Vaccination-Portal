ce214d3cf5741c03bb684f5062051d16164c2951
sai <saipavank2000@gmail.com>
2022-03-03 19:27:35 -0500

    Added additional vaccination information

--- api/middleware.go
+++ api/middleware.go
@@ -6,8 +6,9 @@ import (
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Aceesss-Control-Allow-Origin", "http://localhost:3000")
+		// w.Header().Set("Aceesss-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE")
 
 		next.ServeHTTP(w, r)
--- api/statusHandler.go
+++ api/statusHandler.go
@@ -1,21 +1,23 @@
 package main
 
-import "encoding/json"
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func (app *application) statusHandler(w http.ResponseWriter, r * http.Request) {
-	currentStatus := AppStatus {
-		Status: "Available",
+func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
+	currentStatus := AppStatus{
+		Status:      "Available",
 		Environment: app.config.env,
-		Version: "1.0.0",
+		Version:     "1.0.0",
 	}
 
-	js, err := json.MarshalIndent(currentStatus, "","\t")
+	js, err := json.MarshalIndent(currentStatus, "", "\t")
 	if err != nil {
 		app.logger.Println(err)
 
 	}
-
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(js)
