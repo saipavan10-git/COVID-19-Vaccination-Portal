e7c10fcc69f13029f20db902a1d0e4261ecf3efd
sai <saipavank2000@gmail.com>
2022-03-04 14:54:37 -0500

    Removed gin errors
    There was a problem while using gin router while fixing the cors issue, changed to default router

--- api/routes.go
+++ api/routes.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"net/http"
-	"time"
+	//"time"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
+	//"github.com/gin-contrib/cors"
+	//"github.com/gin-gonic/gin"
 	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	m()
+	//m()
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/vaccine/:id", app.getOneVaccine)
 	router.HandlerFunc(http.MethodGet, "/v1/vaccines", app.getAllVaccines)
@@ -24,18 +24,19 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/logout", app.logout)
 	return app.enableCORS(router)
 }
-func m() {
-	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
-	router.Run()
-}
+
+// func m() {
+// 	router := gin.Default()
+// 	router.Use(cors.New(cors.Config{
+// 		AllowOrigins:     []string{"https://foo.com"},
+// 		AllowMethods:     []string{"PUT", "PATCH"},
+// 		AllowHeaders:     []string{"Origin"},
+// 		ExposeHeaders:    []string{"Content-Length"},
+// 		AllowCredentials: true,
+// 		AllowOriginFunc: func(origin string) bool {
+// 			return origin == "https://github.com"
+// 		},
+// 		MaxAge: 12 * time.Hour,
+// 	}))
+// 	router.Run()
+// }
