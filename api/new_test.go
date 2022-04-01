e962f1b1aae10c04d94446ae0e06f62fb243c4ad
sai <saipavank2000@gmail.com>
2022-04-01 15:34:56 -0400

    Update new_test.go
    #76

--- api/new_test.go
+++ api/new_test.go
@@ -60,20 +60,21 @@ func Test7(t *testing.T) {
 	}
 }
 
-// func Test8(t *testing.T) {
-// 	req := httptest.NewRequest(http.MethodGet, "/v1/vaccines", nil)
-// 	w := httptest.NewRecorder()
-// 	app.recordSignup(w, req)
-// 	res := w.Result()
-// 	defer res.Body.Close()
-// 	data, err := ioutil.ReadAll(res.Body)
-// 	if err != nil {
-// 		t.Errorf("expected error to be nil got %v", err)
-// 	}
-// 	if data == nil {
-// 		t.Errorf("expected data not to be nil")
-// 	}
-// }
+func Test8(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/v1/vaccines", nil)
+	w := httptest.NewRecorder()
+	app.recordSignup(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if data == nil {
+		t.Errorf("expected data not to be nil")
+	}
+}
+
 // func Test9(t *testing.T) {
 // 	req := httptest.NewRequest(http.MethodGet, "/v1/vaccines", nil)
 // 	w := httptest.NewRecorder()
