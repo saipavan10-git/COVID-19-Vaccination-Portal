da8a825f2f8995853a6b4f35901a4ac6a7845c2c
vtanneru <vtanneru@ufl.edu>
2022-04-18 15:50:29 -0400

    vaccine data updating
    Vaccine data Updating with increment and Decrement data

--- covid-tracker/src/pages/AdminPage.js
+++ covid-tracker/src/pages/AdminPage.js
@@ -1,193 +1,182 @@
-import React, { useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Formik, Form } from "formik";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
 function AdminPage(props) {
 
-    //Pop up window
-    const [open, setOpen] = React.useState(false);
+  const [inputPassError, setInputPassError] = useState(false);
+  const [inputEmailError, setInputEmailError] = useState(false);
+  const validate = Yup.object({
+    email: Yup.string()
+      .email('Email is invalid')
+      .required('Email is required'),
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 charaters')
+      .required('Password is required'),
+  })
+
+  let navigate = useNavigate();
+  function sayHello(m) {
+    console.log(JSON.stringify(m));
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(m),
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
     };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+    function Counter() {
+        // Set the initial count state to zero, 0
+        const [count, setCount] = useState(0);
 
-
-    let splitBirthDate = props.birthdate.toString();
-    console.log(props.birthdate);
-    const Item = styled(Paper)(({ theme }) => ({
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
-
-    let navigate = useNavigate();
-    const logOut = async () => {
-        await fetch('http://localhost:4000/v1/logout', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-        });
-        props.setName('');
-        props.setEmail('');
-        props.set2Name('');
-
-        navigate('/');
-    }
-
-    function deleteBooking() {
-        let you = {
-            "x": props.email,
-            "y": props.vaccineId,
-        }
-        const requestOptions = {
-            method: "POST",
-            // body: props.email + " " + props.vaccineId,
-            body: JSON.stringify(you),
+        // Create handleIncrement event handler
+        const handleIncrement = () => {
+            setCount(prevCount => prevCount + 1);
         };
-        fetch("http://localhost:4000/v1/deleteBooking", requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
 
-            });
-        window.location.reload();
-    }
+        //Create handleDecrement event handler
+        const handleDecrement = () => {
+            setCount(prevCount => prevCount - 1);
+        };
+    };
 
-    let showOrNot;
-    let showOrNot2;
-    let logout;
-    // let vaccine;
-    let nothing = ", please sign in";
-    let appointment;
-    if (!props.name) {
-        showOrNot = <Button color="inherit" component={Link}
-            to="/login">Login</Button>
-        showOrNot2 = <Button color="inherit" component={Link}
-            to="/signup">Sign up</Button>
-        // vaccine = <>{props.vaccine}</>
-    } else {
+    // eslint-disable-next-line no-unused-vars
+    const response = fetch("http://localhost:4000/v1/login", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert(
+            data.error.message
+          )
+        } else {
+          navigate('/user');
+          window.location.reload();
+        }
 
-        logout = <Button color="inherit" component={Link} to="/login" onClick={logOut}>log out</Button>
-        // vaccine = <Button color="inherit" component={Link} to="/Certificate" onClick={logOut}>log out</Button>
-    }
+      });
+  }
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <a component={Link} href={"/AdminPage"} className="home">Co<span className="colorchange" >Vi</span>-Book</a>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="inputStyle">
+        <div className="space"></div>
+        <h1>
+          Vaccine-<span className="colorchange">data</span>:
+        </h1>
+        <div style={{ marginTop: "20px" }}></div>
 
-    if (props.vaccineId) {
-        appointment = <><span className="app-text">Your current appointment: <>
-            <div className="space40">Vaccine Name: {props.vaccine}</div>
-            <div className="space40">Dose Num: {props.vaccineNum}</div>
-            <div className="space40">State: {props.state}</div>
-            <div className="space40">Zip Code: {props.zipCode}</div>
-            <div className="space40"></div>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Cancel Appointment
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Are you sure you want to cancel your appointment?"}
-                </DialogTitle>
+        <Formik
 
-                <DialogActions>
-                    <Button onClick={deleteBooking}>
-                        Yes
-                    </Button>
-                    <Button onClick={handleClose}>No</Button>
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => {
+            console.log(values);
+            sayHello(values);
+          }}
+          validationSchema={validate}
+        >
+          {({ values, handleChange, handleSubmit, errors, touched }) => (
+            <Form onSubmit={handleSubmit}>
+              <InputLabel htmlFor="input-with-icon-adornment"></InputLabel>
+              <Input
+                error={inputEmailError}
+                onChange={(e) => { handleChange(e); setInputEmailError(false); }}
+                value={values.email}
+                inputProps={{
+                  style: {
+                    fontSize: "25px",
+                    fontFamily: "Georgia",
+                    color: "#3876e9",
+                    width: "300px",
+                  },
+                }}
+                name="email"
+                placeholder="Email"
+                type="email"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle
+                      style={{ fontSize: "32px", color: "#3876e9" }}
+                    />
+                  </InputAdornment>
+                }
+              />
+              {errors.email && touched.email ? (setInputEmailError(true), (
+                <div style={{ color: "red" }}>{errors.email}</div>
+              )) : null}
 
-                </DialogActions>
-            </Dialog>
+              {/* make space between email and password input */}
+              <div style={{ marginTop: "20px" }}></div>
 
-        </></span></>
-    } else {
-        appointment = <p>You do not have any appointment!</p>
-    }
+              {/* input password */}
+              <InputLabel htmlFor="input-with-icon-adornment"></InputLabel>
+              <Input
+                error={inputPassError}
+                onChange={(e) => { handleChange(e); setInputPassError(false); }}
+                value={values.password}
+                inputProps={{
+                  style: {
+                    fontSize: "25px",
+                    fontFamily: "Georgia",
+                    color: "#3876e9",
+                    width: "300px",
+                  },
+                }}
+                type="password"
+                name="password"
+                placeholder="Password"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockIcon style={{ fontSize: "32px", color: "#3876e9" }} />
+                  </InputAdornment>
+                }
+              />
 
-    return (
-        <>
-            <div>
-                <Box sx={{ flexGrow: 1 }}>
-                    <AppBar>
-                        <Toolbar>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                <a component={Link} href="/user" className="home">Co<span className="colorchange" >Vi</span>-Book</a>
-                            </Typography>
-                            {showOrNot}
-                            {showOrNot2}
-                            <Button color="inherit" component={Link}
-                                to="/list">Vaccine List</Button>
-                            {logout}
-                        </Toolbar>
-                    </AppBar>
-                </Box>
-                <div className="space"></div>
-                <h1>
-                    Welcome {props.name ? props.name : nothing}
+              {errors.password && touched.password ? ((setInputPassError(true),
+                <div style={{ color: "red" }}>{errors.password}</div>
+              )) : null}
 
-                </h1>
-                <Box sx={{ flexGrow: 1 }}  >
-                    <Grid container spacing={2} alignItems="center" justifyContent="center" >
+              {/* make space between email and password input */}
+              <div style={{ marginTop: "40px" }}></div>
 
-                        <Grid item xs={3} md={4}>
-                            <Item>
-                                <p>Below are your basic information:</p>
-                                <p>First Name: {props.name}</p>
-                                <p>Last Name: {props.name2}</p>
-                                <p>Email address: {props.email}</p>
-                                <p>Birth date: { }</p>
+              {/* submit button */}
+              <Button
+                variant="outlined"
+                style={{
+                  fontSize: "18px",
+                  color: "#3876e9",
+                  borderColor: "orange",
+                }}
+                type="submit"
+              >
+                Login
+              </Button>
 
-                            </Item>
-
-                        </Grid>
-                        <Grid item xs={3} md={3}>
-                            <Item>
-                                {appointment}
-
-                            </Item>
-
-                        </Grid>
-                        <Grid item xs={4} md={4}>
-                            <Item>
-                                <h3>What would you like to do?</h3>
-                                <div className="space40"></div>
-                                <Button variant="contained" component={Link}
-                                    to="/list">Schedule an Appointment </Button>
-                                <div className="space40" ></div>
-                                <Button variant="contained" component={Link}
-                                    to="/update">Fill Details for Vaccination Certifcate </Button>
-                                <div className="space40"></div>
-                                {/* <Button variant="contained" component={Link}
-                                    to="/records">Click me to get your certificate! (WIP)</Button> */}
-                                <div className="space40"></div>
-                                <Button component={Link}
-                                    to="/covidcaseall" variant="contained">Covid data dashboard</Button>
-                            </Item>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </div>
-        </>
-    );
-
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
 }
-
 export default AdminPage;
